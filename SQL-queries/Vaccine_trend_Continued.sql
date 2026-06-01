e40296274031dfdb7af6e4f178c0ded7519a72fa
Bhargav Ashok <bhargav.ashok2023@gmail.com>
2026-06-01 12:11:35 -0400

    Fix SQL query syntax for average daily total

--- SQL-queries/Vaccine_trend_Continued.sql
+++ SQL-queries/Vaccine_trend_Continued.sql
@@ -1,6 +1,7 @@
+
+SELECT d.year, AVG(f.daily_total) as avg_daily_total
 FROM fact_vaccination as f
 JOIN dim_date as d ON f.date_key = d.date_key
-SELECT d.year, AVG(f.daily_total) as avg_daily_total
 GROUP BY d.year
 ORDER BY avg_daily_total DESC
 LIMIT 3
